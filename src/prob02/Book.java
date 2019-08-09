025dcc525fb347b9db8ee18aae09a4f8518735c1
hsg <h-sharks@hanmail.net>
2019-08-09 17:05:11 +0900

    20190809 commit

--- src/prob02/Book.java
+++ src/prob02/Book.java
@@ -6,7 +6,34 @@ public class Book {
 	private String author;
 	private int stateCode;
 	
-
+	public void setBookNo(int bookNo) {
+		this.bookNo = bookNo;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public int getBookNo() {
+		return bookNo;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public Book(int bookNo, String title, String author) {
+		this.bookNo = bookNo;
+		this.title = title;
+		this.author = author;
+		this.stateCode = 1;
+	}
+	public void rent() {
+		stateCode = 0;
+		System.out.println(this.bookNo+" 이(가) 대여됐습니다.");
+	}
 	
 	public void print() {
 		System.out.println(
