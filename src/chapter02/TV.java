5aa6326fa8458c41acac6355542588a3accf72b3
hsg <h-sharks@hanmail.net>
2019-08-09 17:05:30 +0900

    20190809 Commit

--- src/chapter02/TV.java
+++ src/chapter02/TV.java
@@ -5,9 +5,56 @@ public class TV {
 	private int volume;
 	private boolean power;
 	
-	public String status() {
-		return "TV [channel=" + channel + ", volume=" + volume + ", power=" + power + "]";
+	public TV() {
+		
 	}
 	
+	public TV(int channel, int volume, boolean power) {
+		this.channel = channel;
+		this.volume = volume;
+		this.power = power;
+	}
 	
+	public void status() {
+		System.out.println("TV [channel=" + channel + ", volume=" + volume + ", power=" + power + "]");
+	}
+
+	public int getChannel() {
+		return channel;
+	}
+
+	public int getVolume() {
+		return volume;
+	}
+
+	public boolean isPower() {
+		return power;
+	}
+	
+	public void power(boolean on) {
+		power = on;
+	}
+	public void channel(int channel) {
+		if(channel >0 && channel <= 255) {
+			this.channel = channel;
+		}
+	}
+	public void channel(boolean up) {
+		if(up == true && (channel >0 && channel <= 255))
+			channel++;
+		else if (up == false && (channel >0 && channel <= 255))
+			channel--;
+	}
+	
+	public void volume(int volume) {
+		if(volume >= 0 && volume <= 255) {
+			this.volume = volume;
+		}
+	}
+	public void volume(boolean up) {
+		if(up == true && (volume >= 0 && volume <= 255))
+			volume++;
+		else if (up == false && (volume >0 && volume <= 255))
+			volume--;		
+	}
 }
